ae0be0fe1f3a9061e18d23c24606af0464537117
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-04 13:39:01 -0600

    add commoncharacterupdate

---
+++ plugins/cata_multiclass_utils.pl
@@ -0,0 +1,30 @@
+sub CommonCharacterUpdate
+{
+    my $client = plugin::val('$client');
+
+    CheckSkillAcquisition();
+    CheckSpellAcquisition($client->GetLevel());
+
+}
+
+# Check if the player is eligible for the first point of a new skill
+sub CheckSkillAcquisition
+{
+    my $client = plugin::val('$client');
+
+    my $max_skill_id = 77;
+
+    foreach my $skill (1..$max_skill_id) {
+        if ($client->MaxSkill($skill) > 0 && $client->GetRawSkill($skill) < 1 && $client->CanHaveSkill($skill)) {
+            $client->SetSkill($skill, 1);
+        }
+    }
+}
+
+sub CheckSpellAcquisition
+{
+    my $client = plugin::val('$client');
+    my $level  = shift || $client->GetLevel(); 
+
+    $client->ScribeSpells($level, $level);
+}

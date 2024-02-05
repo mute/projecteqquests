575ddc32206641f1fb4b4f155b775e4c28bd4e5f
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-05 14:16:10 -0600

    exclude tradeskills

--- plugins/cata_multiclass_utils.pl
+++ plugins/cata_multiclass_utils.pl
@@ -9,15 +9,19 @@ sub CommonCharacterUpdate
 sub CheckSkillAcquisition
 {
     my $client = shift;
-
+    
+    my @exclude_skills = (57,58,59,60,61,63,64,65,66,68,69); 
     my $max_skill_id = 77;
 
-    foreach my $skill (1..$max_skill_id) {
+    foreach my $skill (1..$max_skill_id) {        
+        next if (grep { $_ == $skill } @exclude_skills);
+
         if ($client->MaxSkill($skill) > 0 && $client->GetRawSkill($skill) < 1 && $client->CanHaveSkill($skill)) {
             $client->SetSkill($skill, 1);
         }
     }
 }
+
 
 sub CheckSpellAcquisition
 {

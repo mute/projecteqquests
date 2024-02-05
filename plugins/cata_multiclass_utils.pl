72628f0c3ec1ffe6409f8b6db12d15ef309e1a65
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-05 17:24:02 -0600

    remove auto-skill grant

--- plugins/cata_multiclass_utils.pl
+++ plugins/cata_multiclass_utils.pl
@@ -2,22 +2,4 @@ sub CommonCharacterUpdate
 {
     my $client = shift;
     my $levels_gained = shift;
-
-    CheckSkillAcquisition($client);
-}
-
-sub CheckSkillAcquisition
-{
-    my $client = shift;
-    
-    my @exclude_skills = (57,58,59,60,61,63,64,65,66,68,69); 
-    my $max_skill_id = 77;
-
-    foreach my $skill (1..$max_skill_id) {        
-        next if (grep { $_ == $skill } @exclude_skills);
-
-        if ($client->MaxSkill($skill) > 0 && $client->GetRawSkill($skill) < 1 && $client->CanHaveSkill($skill)) {
-            $client->SetSkill($skill, 1);
-        }
-    }
 }

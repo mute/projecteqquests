ccef767eecf6fb76a7eb6acdafc289430268d042
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-06 13:49:25 -0600

    test

--- thevoida/player.pl
+++ thevoida/player.pl
@@ -2,6 +2,8 @@ sub EVENT_CONNECT {
     if (!$instanceid) {
         quest::debug("Not in an instance!");
         $instance = quest::CreateInstance('thevoida', 0, 360000);
-        quest::MovePCInstance($zoneid, $instance, $client->GetX(), $client->GetY(), $client->GetZ());
+        $client->AssignToInstance($instance);
+
+        $client->MovePCInstance($zoneid, $instance, $client->GetX(), $client()->GetY(), $client->GetZ(), $client->GetHeading());
     }
 }

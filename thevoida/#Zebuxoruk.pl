238a96a89e8e90c79367c832caaba5ec5551b41a
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-08 12:49:31 -0600

    popup

--- thevoida/#Zebuxoruk.pl
+++ thevoida/#Zebuxoruk.pl
@@ -13,7 +13,7 @@ sub EVENT_SAY {
     # Greetings
     # Condition 1: Neither this character nor this account have interacted with this NPC
     if ($text=~/hail/i && !$account_zeb_progress && !$character_zeb_progress) {
-        quest::say("Hail yourself, $base_class_name. Yet another wandering immortal finds this place through the blind abyss, the end of all things. We have met once before, when I challenged the gods, so long ago. In light of that service that you once performed, and will again, I will grant you the ability to [start anew]. Shall we begin?");
+        quest::say("Hail yourself, $base_class_name. Yet another wandering hero finds this place through the blind abyss, the end of all things. We have met once before, when I challenged the gods, so long ago. In light of that service that you once performed, and will again, I will grant you the ability to [start anew]. Shall we begin?");
     }
 
     # Condition 2: This account has interacted with this NPC, but the character has not
@@ -23,23 +23,27 @@ sub EVENT_SAY {
 
     # Condition 3: This character has interacted with Zeb before, but the most recent time was not this character.
     elsif ($text=~/hail/i && $character_zeb_progress && $last_zeb_charname ne $client->GetCleanName()) {
-        quest::say("Back again, immortal? You shift your face as often as I shift the fates, and both of them futile. What do you [require of me]? If there is nothing, then your fate is set. Go [challenge it].");
+        quest::say("Back again, hero? You shift your face as often as I shift the fates, and both of them futile. What do you [require of me]? If there is nothing, then your fate is set. Go [challenge it].");
     }
 
     # Condition 4: This character has interacted with Zeb before, and the most recent time was this character.
     elsif ($text=~/hail/i) {
-        quest::say("Immortal. Your journey continues, much as mine - eternally. What do you [require of me]? If there is nothing, then your fate is set. Go [challenge it].");
+        quest::say("hero. Your journey continues, much as mine - eternally. What do you [require of me]? If there is nothing, then your fate is set. Go [challenge it].");
     }
 
     
     elsif ($text=~/start anew/i) {
         if (plugin::GetClassesCount($client) >= 3) {
             # TODO - Add respec methods
-            quest::say("Your fate is set, immortal. Go [challenge it].");
+            quest::say("Your fate is set, hero. Go [challenge it].");
         } else {
             my $extra_class_list = plugin::GetClassesSelectionString();
             my $deity_message = $deity_name eq "Agnostic" ? "free from the whims of the gods" : "of the so-called god, $deity_name, now lost to the ages";
-            quest::say("Yes, let us explore the multitude of paths before you, lost immortal. As a $base_class_name $deity_message, your first choice, shall it be " . $extra_class_list);
+
+            my $popup_message = "Multiclassing on The Heroes' Journey";
+
+            quest::say("Yes, let us explore the multitude of paths before you, lost hero. As a $base_class_name $deity_message, your first choice, shall it be " . $extra_class_list);
+            quest::popup("Information", $popup_message);
         }
     }
 
@@ -53,7 +57,7 @@ sub EVENT_SAY {
             my $secondary_response;
             
             if ($total_classes_now == 3) {
-                $secondary_response = "Your fate is set, immortal. Go [challenge it].";
+                $secondary_response = "Your fate is set, hero. Go [challenge it].";
                 if (!$account_zeb_progress) {
                     quest::set_data(plugin::GetAccountKey() . "zeb-progress", 1);
                 }
@@ -61,12 +65,12 @@ sub EVENT_SAY {
             } else {
                 # Assuming you have a method to generate the selection string for the remaining classes
                 my $selection_string = plugin::GetClassesSelectionString();
-                $secondary_response = "Your second and final choice, immortal. Choose wisely: $selection_string";
+                $secondary_response = "Your second and final choice, hero. Choose wisely: $selection_string";
             }
 
             quest::say("Indeed - it is done. $secondary_response");
         } else {
-            quest::say("Alas, I am unable to do so. Your fate is set, immortal. Go [challenge it].");
+            quest::say("Alas, I am unable to do so. Your fate is set, hero. Go [challenge it].");
         }
     }
 
@@ -76,11 +80,11 @@ sub EVENT_SAY {
 
     elsif ($text=~/return/i) {
         if (plugin::GetClassesCount($client) == 3) {
-            quest::say("Farewell, immortal. I'm sure that you'll find your way back here again.");
+            quest::say("Farewell, hero. I'm sure that you'll find your way back here again.");
 
             plugin::ReturnToZone($client);
         } else {
-            quest::say("You are not yet ready to leave, immortal. There is still time to [start anew].");
+            quest::say("You are not yet ready to leave, hero. There is still time to [start anew].");
         }
     }
 }

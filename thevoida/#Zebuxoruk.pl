3641547f6a5eb5b7d0956020486172fc89137d9e
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-09 23:13:57 -0600

    test

--- thevoida/#Zebuxoruk.pl
+++ thevoida/#Zebuxoruk.pl
@@ -10,7 +10,7 @@ sub EVENT_SAY {
 
     quest::set_data(plugin::GetAccountKey() . "zeb-last-charname", $client->GetCleanName());
 
-    quest::debug("HasMeleeClass: " . plugin::HasMeleeClass($client) . ":" . plugin::IsMeleeClass(2));
+    quest::debug("HasMeleeClass: " . plugin::HasMeleeClass($client) . ":" . plugin::IsMeleeClass(3));
 
     # Greetings
     # Condition 1: Neither this character nor this account have interacted with this NPC

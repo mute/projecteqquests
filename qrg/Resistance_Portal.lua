9ca2d77f9286a7ffd0adf612be5b2420dd058e4a
mute <natanx@gmail.com>
2024-02-17 20:08:54 -0800

    Add Sungold Grasslands

--- qrg/Resistance_Portal.lua
+++ qrg/Resistance_Portal.lua
@@ -600,7 +600,8 @@ local Allowed_Zones = {
     [1] = { "qeytoqrg", 4, 0, 0, 83, 508, 0, 0, 5, 0, "The Qeynos Hills" },
     [2] = { "misty", 33, 0, 0, 0, 0, 7, 0, 10, 0, "Mosswood" },
     [3] = { "kurn", 97, 0, 0, 0, 0, 7, 0, 15, 0, "Mosswood Tower" },
-    [4] = { "crushbone", 59, 0, 0, 120, -330, -178, 0, 25, 0, "Crushbone Keep" }
+    [4] = { "crushbone", 59, 0, 0, 120, -330, -178, 0, 25, 0, "Crushbone Keep" },
+    [5] = { "moors", 395, 0, 0, 3263, -626, -20, 0, 0, 12, "Sungold Grasslands" },
 }
 
 -- [id] = { "short_name", zoneidnumber, version, maxclients, safe_x, safe_y, safe_z, safe_heading, min_level, expansion,"long_name"}
@@ -615,7 +616,7 @@ local Leveling_Zones = {
         [1] = { "kurn", 97, 0, 0, 0, 0, 7, 0, 15, 0, "The Mosswood Tower" },
     },
     ['25-30'] = {
-        [1] = { "crushbone", 58, 0, 0, 158, -644, 4, 0, 25, 0, "Crushbone Keep" },
+        [1] = { "crushbone", 59, 0, 0, 120, -330, -178, 0, 25, 0, "Crushbone Keep" },
         [2] = { "moors", 395, 0, 0, 3263, -626, -20, 0, 25, 0, "Sungold Grasslands" },
     }
 }
@@ -687,7 +688,7 @@ function event_say(e)
             local zoneShortName = zoneData[1]
             local zoneLongName = zoneData[11]
 
-            if e.message:findi(zoneLongName) or e.message:findi(zoneShortName) then
+            if e.message:findi(zoneLongName) then
                 -- [id] = { "short_name", zoneidnumber, version, maxclients, safe_x, safe_y, safe_z, safe_heading, min_level, expansion,"long_name"}
                 local recordID = zoneData[1]
                 local zoneID = zoneData[2]

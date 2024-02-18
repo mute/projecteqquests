d2247f45346f2c72a14a25f74aeb7b7cb068caef
mute <natanx@gmail.com>
2024-02-17 20:57:38 -0800

    Adds Blixkin quest in Surefall

--- qeytoqrg/Blixkin.lua
+++ qeytoqrg/Blixkin.lua
@@ -1,8 +1,8 @@
 function event_say(e)
     if(e.message:findi("hail")) then
-        if not e.other:IsTaskActive(5) and not e.other:IsTaskCompleted(5) then
+        if not self.client:IsTaskActive(5) and not self.client:IsTaskCompleted(5) then
             e.self:Say("TOAST DID NOT WRITE THE QUEST TEXT FOR THIS YET");
-            e.other:AssignTask(5);
+            e.other:AssignTask(5,e.self:GetID());    
         end
     end
 end

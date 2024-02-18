55a8eae0188fc14f9783eac2da0d9dd41508c478
mute <natanx@gmail.com>
2024-02-18 14:12:52 -0800

    Fix Blixkin in qeytoqrg maybe?

--- qeytoqrg/Blixkin.lua
+++ qeytoqrg/Blixkin.lua
@@ -2,7 +2,7 @@ function event_say(e)
     if(e.message:findi("hail")) then
         if not e.other:IsTaskActive(5) and not e.other:IsTaskCompleted(5) then
             e.self:Say("TOAST DID NOT WRITE THE QUEST TEXT FOR THIS YET");
-            e.other:AssignTask(5,e.self:GetID());
+            e.other:AssignTask(5);
         end
     end
 end

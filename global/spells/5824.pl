6b9746c0ea846515b45a76a2d59c385377204439
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-20 16:10:04 -0600

    test

---
+++ global/spells/5824.pl
@@ -0,0 +1,14 @@
+# Bazaar Portal
+sub EVENT_SPELL_EFFECT_CLIENT {
+	# Spell-EVENT_SPELL_EFFECT_CLIENT
+	# Exported event variables
+	quest::debug("spell_id " . $spell_id);
+	quest::debug("caster_id " . $caster_id);
+	quest::debug("tics_remaining " . $tics_remaining);
+	quest::debug("caster_level " . $caster_level);
+	quest::debug("buff_slot " . $buff_slot);
+	quest::debug("spell " . $spell);
+
+    my $x = $client->GetX();
+    quest::debug("X: $x");
+}

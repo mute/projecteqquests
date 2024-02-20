3c3a80f4b7579e2a8bdb8f9bf51d5daa1058dac1
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-20 16:19:13 -0600

    set return coords from bazaar gate

--- global/spells/5824.pl
+++ global/spells/5824.pl
@@ -1,17 +1,11 @@
 # Bazaar Portal
 sub EVENT_SPELL_EFFECT_CLIENT {
-	# Spell-EVENT_SPELL_EFFECT_CLIENT
-	# Exported event variables
-	quest::debug("spell_id " . $spell_id);
-	quest::debug("caster_id " . $caster_id);
-	quest::debug("tics_remaining " . $tics_remaining);
-	quest::debug("caster_level " . $caster_level);
-	quest::debug("buff_slot " . $buff_slot);
-	quest::debug("spell " . $spell);
-
     my $client = plugin::val('$client');
-    my $x = $client->GetX();
-    $client->Message(13,"X: $x");
-    quest::debug("X: $x");
 
+    if ($zoneid != 151) {
+        $client->SetBucket("Return-X", $client->GetX());
+        $client->SetBucket("Return-Y", $client->GetY());
+        $client->SetBucket("Return-Z", $client->GetZ());
+        $client->SetBucket("Return-H", $client->GetHeading());
+    }
 }

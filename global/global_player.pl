87432495afe60cd9f2bf964beee8084806dc3c7b
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-20 16:52:50 -0600

    test

--- global/global_player.pl
+++ global/global_player.pl
@@ -32,9 +32,17 @@ sub EVENT_DISCOVER_ITEM {
 }
 
 sub EVENT_ZONE {
-	plugin::CheckWorldWideBuffs($client);
+	# Player-EVENT_ZONE
+	# Exported event variables
+	quest::debug("from_zone_id " . $from_zone_id);
+	quest::debug("from_instance_id " . $from_instance_id);
+	quest::debug("from_instance_version " . $from_instance_version);
+	quest::debug("target_zone_id " . $target_zone_id);
+	quest::debug("target_instance_id " . $target_instance_id);
+	quest::debug("target_instance_version " . $target_instance_version);
 
-    quest::debug("We are zoning out of: $zoneid");
+    
+	plugin::CheckWorldWideBuffs($client);
 }
 
 sub EVENT_WARP {

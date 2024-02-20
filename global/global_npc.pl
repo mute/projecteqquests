742aafe0588e2b084b77f10267d78bcc8a5fb725
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-20 15:52:43 -0600

    poggers

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,32 +1,84 @@
 sub EVENT_SAY {
     my $classes = $client->GetClassesBitmask();
+    my $player_class_id = $npc->GetClass() - 19;
+    my $class_name = quest::getclassname($player_class_id);
     if ($npc->GetClass() >= 20 && $npc->GetClass() <= 35) {
         if ($text=~/hail/i) {
-            my $player_class_id = $npc->GetClass() - 19;
-            my $class_name = quest::getclassname($player_class_id);
+            my $select_string = quest::saylink("class_select", 1, "become a $class_name");
             my %class_greetings = (
-                1 => "Ah, a courageous soul approaches. Are you here to embrace the discipline and strength required to [" . quest::saylink("class_select", 1, "become a Warrior") . "]?",
-                2 => "Blessings upon you, child. The light guides you to me; is it your wish to [" . quest::saylink("class_select", 1, "become a Cleric") . "] and serve the divine?",
-                3 => "Honor and valor shine from your eyes. Are you destined to [" . quest::saylink("become_a_Paladin", 1, "class_select a Paladin") . "], a righteous defender of the light?",
-                4 => "The winds whisper of a new guardian. Is your heart called to the wilds, to [" . quest::saylink("class_select", 1, "become a Ranger") . "], protector of nature?",
-                5 => "A shadow looms near. Is it your fate to command the darkness and [" . quest::saylink("class_select", 1, "become a Shadow Knight") . "]?",
-                6 => "The essence of nature surrounds you. Are you ready to [" . quest::saylink("become_a_Druid", 1, "class_select a Druid") . "], guardian of the balance?",
-                7 => "Discipline and inner strength are your allies. Do you seek the path to [" . quest::saylink("class_select", 1, "become a Monk") . "], master of martial arts?",
-                8 => "A melody accompanies your steps. Do you feel the rhythm calling you to [" . quest::saylink("class_select", 1, "become a Bard") . "], the voice of inspiration?",
-                9 => "Cunning and silence are your markers. Are you prepared to [" . quest::saylink("class_select", 1, "class_select a Rogue") . "], master of stealth and treachery?",
-                10 => "The spirits whisper of a new journey. Is it time for you to [" . quest::saylink("class_select", 1, "become a Shaman") . "], a conduit of the spirit world?",
-                11 => "A chill of the grave precedes you. Will you embrace the dark arts and [" . quest::saylink("class_select", 1, "become a Necromancer") . "]?",
-                12 => "Arcane energies pulse around you. Is your destiny to [" . quest::saylink("class_select", 1, "become a Wizard") . "], master of the elements?",
-                13 => "Creation's essence swirls around you. Are you called to [" . quest::saylink("class_select", 1, "become a Magician") . "], summoner of the arcane?",
-                14 => "Your presence bends reality. Are you ready to [" . quest::saylink("class_select", 1, "become an Enchanter") . "], weaver of illusions and mind control?",
-                15 => "The call of the wild strengthens. Will you heed the call and [" . quest::saylink("class_select", 1, "become a Beastlord") . "], melding the power of beasts and combat?",
-                16 => "Rage burns within your spirit. Do you wish to unleash this power and [" . quest::saylink("class_select", 1, "become a Berserker") . "], a warrior of frenzy?"
+                1 => "Ah, a courageous soul approaches. Are you here to embrace the discipline and strength required to [$select_string]?",
+                2 => "Blessings upon you, child. The light guides you to me; is it your wish to [$select_string] and serve the divine?",
+                3 => "Honor and valor shine from your eyes. Are you destined to [$select_string], a righteous defender of the light?",
+                4 => "The winds whisper of a new guardian. Is your heart called to the wilds, to [$select_string], protector of nature?",
+                5 => "A shadow looms near. Is it your fate to command the darkness and [$select_string]?",
+                6 => "The essence of nature surrounds you. Are you ready to [$select_string], guardian of the balance?",
+                7 => "Discipline and inner strength are your allies. Do you seek the path to [$select_string], master of martial arts?",
+                8 => "A melody accompanies your steps. Do you feel the rhythm calling you to [$select_string], the voice of inspiration?",
+                9 => "Cunning and silence are your markers. Are you prepared to [$select_string], master of stealth and treachery?",
+                10 => "The spirits whisper of a new journey. Is it time for you to [$select_string], a conduit of the spirit world?",
+                11 => "A chill of the grave precedes you. Will you embrace the dark arts and [$select_string]?",
+                12 => "Arcane energies pulse around you. Is your destiny to [$select_string], master of the elements?",
+                13 => "Creation's essence swirls around you. Are you called to [$select_string], summoner of the arcane?",
+                14 => "Your presence bends reality. Are you ready to [$select_string], weaver of illusions and mind control?",
+                15 => "The call of the wild strengthens. Will you heed the call and [$select_string], melding the power of beasts and combat?",
+                16 => "Rage burns within your spirit. Do you wish to unleash this power and [$select_string], a warrior of frenzy?"
             );
             
             my $greeting = $class_greetings{$player_class_id} // "Greetings, traveler. Are you seeking guidance or knowledge?";
             if (!($classes & plugin::GetClassBitmask($player_class_id)) && plugin::GetClassesCount($client) < 3) {
                 plugin::NPCTell($greeting);
             }
+        }
+    }
+
+    if ($text eq "class_select") {
+        my $class_name = quest::getclassname($npc->GetClass() - 19);
+        my $confirm_link = quest::saylink("class_confirm", 1, "Are you ready to commit to the path of the $class_name?");
+
+        my %class_specific_messages = (
+            1 => "The path of the Warrior is arduous and demanding, requiring unwavering courage and strength. " . 
+                "Once chosen, this path is your destiny, only to be reversed at the whims of the gods. " . $confirm_link,
+            2 => "Embracing the Cleric's way means dedicating your life to the divine, serving as a beacon of light and healing. " .
+                "This sacred commitment is binding, revered by the gods themselves. " . $confirm_link,
+            3 => "The Paladin stands as a beacon of hope, blending the might of arms with the purity of faith. " .
+                "Are you prepared to defend the light and uphold justice, knowing such a choice is guided by the gods? " . $confirm_link,
+            4 => "Rangers protect the balance of nature, a path filled with peril and beauty. " .
+                "If your heart is true to the wild, confirm your dedication to become one with nature and its guardians. " . $confirm_link,
+            5 => "Shadow Knights wield the power of darkness and fear. " .
+                "This path is fraught with danger and moral ambiguity. Only the most resolute may walk it, and once chosen, it is rarely abandoned. " . $confirm_link,
+            6 => "Druids are the guardians of nature, harmonizing the forces of life and growth. " .
+                "To walk this path is to become one with the earth itself. Are you ready to embrace this eternal bond? " . $confirm_link,
+            7 => "The Monk's discipline is forged from inner strength and relentless training. " .
+                "Embrace this path with the understanding that it demands complete devotion, a devotion that is recognized by the gods. " . $confirm_link,
+            8 => "Bards are the heart of any fellowship, weaving magic and music into powerful symphonies. " .
+                "If you feel the song within your soul, affirm your desire to live a life of melody and adventure. " . $confirm_link,
+            9 => "Rogues thrive in the shadows, where cunning and agility are the keys to survival. " .
+                "Is your spirit attuned to the silent whispers of the dark? Confirm your path and step into the world unseen. " . $confirm_link,
+            10 => "Shamans act as intermediaries between the physical and spirit worlds. " .
+                "If you are called to bridge these realms, affirm your commitment to the spiritual journey ahead. " . $confirm_link,
+            11 => "Necromancers command the forces of death and decay. " .
+                "This dark path is not chosen lightly, for its course is irrevocable, shadowed by the oversight of the gods themselves. " . $confirm_link,
+            12 => "Wizards master the arcane, wielding the raw forces of magic. " .
+                "If you seek to harness these elemental powers, confirm your resolve to tread a path fraught with danger and discovery. " . $confirm_link,
+            13 => "Magicians shape reality, summoning creatures and objects from the ether. " .
+                "Are you prepared to command the very fabric of existence, knowing such power is watched closely by the divine? " . $confirm_link,
+            14 => "Enchanters twist the minds and reality itself, with wisdom and subtlety. " .
+                "If you choose to weave the strands of fate, know that this path is as binding as the spells you will cast. " . $confirm_link,
+            15 => "Beastlords bond with the spirits of animals, embodying their primal essence. " .
+                "This sacred pact with nature is eternal, guided by the spirits and overseen by the gods. Are you ready to accept this union? " . $confirm_link,
+            16 => "Berserkers unleash their inner fury, a force of pure, unbridled power. " .
+                "This path of rage is relentless and all-consuming. Confirm if you are ready to embrace the storm within, under the gaze of the gods. " . $confirm_link
+        );
+
+        my $class_message = $class_specific_messages{$player_class_id} // "The path before you is significant, a choice that once made, is not easily undone. The gods watch over your decision. " . $confirm_link;
+        plugin::NPCTell($class_message);
+
+        if ($player_class_id == 8) { # Special Handling for Bard
+            $client->Message(13, "WARNING: You will be immediately disconnected so that your base class can be changed to Bard. " .
+                                "All class combinations that include Bard must be base class Bard.");
+        } elsif (plugin::IsMeleeClass($player_class_id) && !plugin::IsMeleeClass($client->GetClass())) { # Special handling for Melee Classes
+            $client->Message(13, "WARNING: You will be immediately disconnected so that your base class can be changed to $class_name. " .
+                                "All class combinations that include a melee or Hybrid must have a Melee or Hybrid as their base class.");
         }
     }
 }

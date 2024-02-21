00aabc6e7b52bf23f942b4ebce802ec041fe2f4f
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-21 17:15:09 -0600

    test

--- global/global_player.pl
+++ global/global_player.pl
@@ -6,24 +6,6 @@ sub EVENT_ENTERZONE {
     plugin::CheckWorldWideBuffs($client);
     plugin::CommonCharacterUpdate($client);
 
-    my $ReturnX = $client->GetBucket("Return-X");
-    my $ReturnY = $client->GetBucket("Return-Y");
-    my $ReturnZ = $client->GetBucket("Return-Z");
-    my $ReturnH = $client->GetBucket("Return-H");
-    my $ReturnZone = $client->GetBucket("Return-Zone");
-
-    if ($ReturnX && $ReturnY && $ReturnZ && $ReturnH && $ReturnZone) {
-        if ($from_zone_id == 151) {
-            $client->MovePC($ReturnZone, $ReturnX, $ReturnY, $ReturnZ, $ReturnH);
-        } elsif ($from_zone_id != $ReturnZone) {
-            $client->DeleteBucket("Return-X");
-            $client->DeleteBucket("Return-Y");
-            $client->DeleteBucket("Return-Z");
-            $client->DeleteBucket("Return-H");
-            $client->DeleteBucket("Return-Zone");
-        }
-    }
-
     if (!plugin::IsEligibleForZone($client, $zonesn)) {
 		$client->Message(4, "Your vision blurs. You lose conciousness and wake up in a familiar place.");
 		$client->MovePC(151, 185, -835, 4, 390); # Bazaar Safe Location.
@@ -74,7 +56,24 @@ sub EVENT_DISCOVER_ITEM {
 }
 
 sub EVENT_ZONE {
+    my $ReturnX = $client->GetBucket("Return-X");
+    my $ReturnY = $client->GetBucket("Return-Y");
+    my $ReturnZ = $client->GetBucket("Return-Z");
+    my $ReturnH = $client->GetBucket("Return-H");
+    my $ReturnZone = $client->GetBucket("Return-Zone");
 
+    if ($ReturnX && $ReturnY && $ReturnZ && $ReturnH && $ReturnZone) {
+        if ($from_zone_id == 151) {
+            $client->MovePC($ReturnZone, $ReturnX, $ReturnY, $ReturnZ, $ReturnH);
+            return $ReturnZone;
+        } elsif ($from_zone_id != $ReturnZone) {
+            $client->DeleteBucket("Return-X");
+            $client->DeleteBucket("Return-Y");
+            $client->DeleteBucket("Return-Z");
+            $client->DeleteBucket("Return-H");
+            $client->DeleteBucket("Return-Zone");
+        }
+    }
 
 	plugin::CheckWorldWideBuffs($client);
 }

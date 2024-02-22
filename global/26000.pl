377be50407ab2598be01f5e9428ad508de0f7b5a
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-22 15:35:15 -0600

    there we go

--- global/26000.pl
+++ global/26000.pl
@@ -1,15 +1,40 @@
 sub EVENT_SPAWN {
   quest::settimer(1, 1200);
-
   quest::emote("rises from the corpse and stares around, as if waiting...");
+
+  $npc->SetGender(int(rand(2)));
 }
 
 sub EVENT_SAY {
-  if ($text =~ /hail/i) {
     my $flag_stage = $npc->GetBucket("Stage-Name");
     my $flag_name  = $npc->GetBucket("Flag-Name");
-    plugin::set_subflag($client, $flag_stage, $flag_name);
-  }
+
+    if ($text =~ /hail/i) {
+        
+        if ($flag_stage eq 'RoK') {
+            plugin::BlueText("Your mind flashes with recollections of savage lands; dense jungles, desolate swamps, and fiery wastes.");
+        }
+        elsif ($flag_stage eq 'SoV') {
+            plugin::BlueText("You almost feel a chill in your bones as your mind fills with visions of endless ice plains, and fortresses filled with Giants and Dragons alike.");
+        }
+        elsif ($flag_stage eq 'SoL') {
+            plugin::BlueText("Your mind recoils at the eldritch horror; dark shadows whisper to you of rites and mysteries alike.");
+        }
+        elsif ($flag_stage eq 'PoP') {
+            plugin::BlueText("You sense a disturbance in the planes; a power grows near... again.");
+        }
+        elsif ($flag_stage eq 'GoD') {
+            plugin::BlueText("You remember an island lost to the mists, conqurered and shattered, yet its people's will remains unbroken.");
+        }
+        elsif ($flag_stage eq 'OoW') {
+            plugin::BlueText("You recall the Overlord of the invasion, sitting in his throne as he surveys the worlds he regards as prey.");
+        }
+        elsif ($flag_stage eq 'DoN') {
+            plugin::BlueText("You recall the ancient dragons, and grow fearful at the prospect of them stirring once more.");
+        }
+        
+        plugin::set_subflag($client, $flag_stage, $flag_name);
+    }
 }
 
 sub EVENT_TIMER {
--- global/global_npc.pl
+++ global/global_npc.pl
@@ -118,6 +118,22 @@ sub EVENT_DEATH {
     CHECK_CHARM_STATUS(); 
 }
 
+sub EVENT_DEATH_COMPLETE {
+    if (plugin::subflag_exists($npc->GetCleanName())) {
+        my $flag_mob = quest::spawn2(26000, 0, 0, $x, $y, ($z + 10), 0); # Spawn a flag mob
+        my $new_npc = $entity_list->GetNPCByID($flag_mob);
+        
+        $new_npc->SetBucket("Flag-Name", $npc->GetCleanName(), "1200s");
+        $new_npc->SetBucket("Stage-Name", plugin::get_subflag_stage($npc->GetCleanName()), "1200s");
+    }
+}
+
+sub EVENT_KILLED_MERIT {
+    if (plugin::subflag_exists($npc->GetCleanName())) {
+        plugin::set_subflag($client, plugin::get_subflag_stage($npc->GetCleanName()), $npc->GetCleanName());
+    }
+}
+
 sub UPDATE_PET_BAG {    
     #quest::debug("--Syncronizing Pet Inventory--");
     my $owner = $npc->GetOwner()->CastToClient();

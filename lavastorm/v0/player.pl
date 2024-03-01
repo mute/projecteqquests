a7c324d7eea1d26c91396b5db9d53176367f1fa7
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-01 17:07:47 -0600

    f

--- lavastorm/v0/player.pl
+++ lavastorm/v0/player.pl
@@ -1,3 +1,7 @@
 sub EVENT_ENTER_ZONE {
     $client->MovePC($zoneid, quest::GetInstanceID($zonesn, 1), $client->GetX(), $client->GetY(), $client->GetZ(), $client->GetHeading());
 }
+
+sub EVENT_CONNECT {
+    $client->MovePC($zoneid, quest::GetInstanceID($zonesn, 1), $client->GetX(), $client->GetY(), $client->GetZ(), $client->GetHeading());
+}

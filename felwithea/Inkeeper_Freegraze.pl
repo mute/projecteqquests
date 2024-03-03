0b54c3c776e21b5dd905cae859b5a186de3619d4
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-02 21:45:39 -0600

    test

--- felwithea/Inkeeper_Freegraze.pl
+++ felwithea/Inkeeper_Freegraze.pl
@@ -1,4 +1,5 @@
 sub EVENT_SAY {
+	quest::debug("bucket: " . $npc->GetBucket("Tolon_Nurbyte"));
     if ($text=~/tolon nurbyte/i && !$npc->GetBucket("Tolon_Nurbyte")) {
         quest::say("So you are inquiring about Mister Tolon Nurbyte, eh? He is on the second floor, last door on the right. You two had best not be up to any mischief. The pair of you look a little shifty for the kingdom of Felwithe.");
         #:: Spawn one and only one Northern Felwithe >> Tolon_Nurbyte (61095), without guild war or pathing grid, at the specified location

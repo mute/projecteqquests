776d75b41068fda61c732d09dd46e6e62865aa65
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-06 17:52:48 -0600

    add flags back to world spawns

--- permafrost/Lady_Vox.pl
+++ permafrost/Lady_Vox.pl
@@ -81,3 +81,19 @@ sub WIPE_AGGRO {
     quest::stoptimer("leash");
     quest::setnexthpevent(96);
 }
+
+sub EVENT_DEATH_COMPLETE {
+    if (plugin::subflag_exists($npc->GetCleanName())) {
+        my $flag_mob = quest::spawn2(26000, 0, 0, $x, $y, ($z + 10), 0); # Spawn a flag mob
+        my $new_npc = $entity_list->GetNPCByID($flag_mob);
+        
+        $new_npc->SetBucket("Flag-Name", $npc->GetCleanName(), "1200s");
+        $new_npc->SetBucket("Stage-Name", plugin::get_subflag_stage($npc->GetCleanName()), "1200s");
+    }
+}
+
+sub EVENT_KILLED_MERIT {
+    if (plugin::subflag_exists($npc->GetCleanName())) {
+        plugin::set_subflag($client, plugin::get_subflag_stage($npc->GetCleanName()), $npc->GetCleanName());
+    }
+}

ce62fcf99ca9c4dec3a99c8f3d6a1ff3fb20d61d
mute <natanx@gmail.com>
2024-03-29 10:14:19 -0700

    Adds a basic encounter script for GRANUS

---
+++ qrg/2000988.lua
@@ -0,0 +1,26 @@
+-- Granus of Vegarlson encounter script
+
+function event_spawn(e)
+    eq.set_next_hp_event(60)
+end
+
+-- Totem locations
+local totem_id = 3061;
+local totem_locs = {
+    [1] = {-492, 50, 41},
+    [2] = {-510, 72, 41},
+    [3] = {-535, 53, 41}
+}
+
+function event_hp(e)
+    if (e.hp_event == 60) then
+        e.self:Say("The earth surrounds me. I will not cave so easily.");
+        e.self:Emote(" hardens as earthen totems rise from the ground.")
+        for i = 1, 3 do
+            eq.spawn2(totem_id, 0, 0, locs[i][0], locs[i][1], locs[i][2]);            
+        end
+        eq.set_next_hp_event(10);
+    elseif (e.hp_event == 10) then
+        e.self:Say("Well done, young one. This is only just the beginning.");
+    end
+end

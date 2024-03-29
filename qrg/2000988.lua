004ef311492283bd487403b137c8b96ae3d298e3
mute <natanx@gmail.com>
2024-03-29 11:38:34 -0700

    gzuz gzuz

--- qrg/2000988.lua
+++ qrg/2000988.lua
@@ -19,7 +19,7 @@ function Totem_Death()
         granus:SetSpecialAbility(35, 0); --turn on immunity
 
         -- Attack normal speed again
-        granus:ModifyNPCStat("attack_delay", "3000");
+        eq.modify_npc_stat("attack_delay", "0");
 
         granus:Emote("'s earthen shell cracks and fractures.");
     end
